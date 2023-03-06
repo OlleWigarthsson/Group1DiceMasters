e2564c7395361a13495a051d212bf2a4c84660b5
Jonas <jonasmagnusson51@gmail.com>
2023-03-06 13:13:38 +0100

    Added First Menu

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "menu.h"
 int main(){
+	printGameMenu();
 
-	printf("Hello World\n");
 }
---
+++ src/menu.c
@@ -0,0 +1,10 @@
+#include "menu.h"
+
+void printGameMenu(){
+	printf("1. Single Player\n");
+	printf("2. Multiplayer\n");
+	printf("3. View Highscores\n");
+	printf("4. Settings\n");
+	printf("5. Quit\n");
+
+}
