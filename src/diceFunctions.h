fcd6817ffd22d2075ec4b4e1b5f5392167fcbad3
Åsa Hildebrand <hildebrand86@hotmail.com>
2023-03-07 13:21:38 +0100

    Added subpoolOfDice

--- src/diceFunctions.h
+++ src/diceFunctions.h
@@ -4,6 +4,7 @@ void addDice(DicePool *pool, Dice dice);
 void mergePools(DicePool *pool1, DicePool pool2);
 int rollDice();
 void rollPool(DicePool *pool);
+int subpoolOfDice(DicePool* pool, int faceValue);
 
 //Function to sum dices
 int sumOfDicepool(Dice diceArray[], int numberOfDice);
