f76b4dc1707c156480a32df2880bbca8082cae7a
Linnea Domberg <linnea@domberg.se>
2023-03-08 15:03:34 +0100

    Uppdatering findFullHous

--- src/diceFunctions.h
+++ src/diceFunctions.h
@@ -9,3 +9,12 @@ int subpoolOfDice(DicePool* pool, int faceValue);
 //Function to sum dices
 int sumOfDicepool(Dice diceArray[], int numberOfDice);
 
+// A bubble sorting, from smallest to biggest
+void soringSmallBig (DicePool *pool);
+
+// A bubble sorting, from biggest to smallest
+void sortingBigSmall(DicePool *pool);
+
+// Funktion to see if we have a full house or not
+int findFullHouse(DicePool *pool);
+
